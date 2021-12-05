8677b5c8e28104b0c4dad15de919956f943e465a
brikehn <thebriankwon@gmail.com>
2021-12-05 15:58:42 -0500

    update styling, add linking to external apps

--- src/components/Socials/SocialCard/index.js
+++ src/components/Socials/SocialCard/index.js
@@ -4,13 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClone as farClone } from '@fortawesome/free-regular-svg-icons'
 import {
   faClone as fasClone,
-  faCheck
+  faCheck,
+  faArrowRight
 } from '@fortawesome/free-solid-svg-icons'
 import Tooltip from '../Tooltip/index'
 
-const SocialCard = ({ icon, label, color }) => {
+const SocialCard = ({ icon, label, color, link }) => {
   const [copyIcon, setCopyIcon] = useState(farClone)
   const [copyIconOpacity, setCopyIconOpacity] = useState(0.4)
+  const [arrowIconOpacity, setArrowIconOpacity] = useState(0.4)
   const [tooltipActive, setTooltipActive] = useState(false)
 
   const handleCopyClick = () => {
@@ -33,25 +35,43 @@ const SocialCard = ({ icon, label, color }) => {
         {label.name}
         <span style={{ color: '#ffffffaa' }}>{label.subname}</span>
       </span>
-      <span
-        className="copy-btn tooltip"
-        onMouseEnter={() => {
-          if (copyIcon !== faCheck) {
-            setCopyIcon(fasClone)
-            setCopyIconOpacity(1)
-          }
-        }}
-        onMouseLeave={() => {
-          if (copyIcon !== faCheck) {
-            setCopyIcon(farClone)
-            setCopyIconOpacity(0.4)
-          }
-        }}
-        onClick={handleCopyClick}
-      >
-        {tooltipActive && <Tooltip text="COPIED" />}
-        <FontAwesomeIcon icon={copyIcon} opacity={copyIconOpacity} />
-      </span>
+      {link
+        ? (
+        <span
+          className="arrow-btn"
+          onMouseEnter={() => {
+            setArrowIconOpacity(1)
+          }}
+          onMouseLeave={() => {
+            setArrowIconOpacity(0.4)
+          }}
+        >
+          <a href={link}>
+            <FontAwesomeIcon icon={faArrowRight} opacity={arrowIconOpacity} />
+          </a>
+        </span>
+          )
+        : (
+        <span
+          className="copy-btn tooltip"
+          onMouseEnter={() => {
+            if (copyIcon !== faCheck) {
+              setCopyIcon(fasClone)
+              setCopyIconOpacity(1)
+            }
+          }}
+          onMouseLeave={() => {
+            if (copyIcon !== faCheck) {
+              setCopyIcon(farClone)
+              setCopyIconOpacity(0.4)
+            }
+          }}
+          onClick={handleCopyClick}
+        >
+          {tooltipActive && <Tooltip text="COPIED" />}
+          <FontAwesomeIcon icon={copyIcon} opacity={copyIconOpacity} />
+        </span>
+          )}
     </div>
   )
 }
@@ -59,7 +79,8 @@ const SocialCard = ({ icon, label, color }) => {
 SocialCard.propTypes = {
   icon: PropTypes.object,
   label: PropTypes.object,
-  color: PropTypes.string
+  color: PropTypes.string,
+  link: PropTypes.string
 }
 
 export default SocialCard
--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -5,11 +5,13 @@ import SocialCard from './SocialCard'
 import { ReactComponent as DiscordIcon } from '../../assets/images/Discord-Logo-White.svg'
 import { ReactComponent as SteamIcon } from '../../assets/images/Steam-Logo.svg'
 import { ReactComponent as RiotGamesIcon } from '../../assets/images/Riot_Games-Logo.svg'
+import { ReactComponent as SpotifyIcon } from '../../assets/images/Spotify-Logo.svg'
 
 const colors = {
   discord: '#5865F2',
   steam: '#000000',
-  riotgames: '#d13639'
+  riotgames: '#d13639',
+  spotify: '#191414'
 }
 
 const Socials = () => {
@@ -19,16 +21,24 @@ const Socials = () => {
         icon={<DiscordIcon width="32" height="32" />}
         label={{ name: 'hana', subname: '#0001' }}
         color={colors.discord}
+        link="discord:///users/244251015069564938"
       />
       <SocialCard
         icon={<SteamIcon width="32" height="32" />}
         label={{ name: 'hanabanana' }}
         color={colors.steam}
+        link="steam://openurl/https://steamcommunity.com/id/hanasama96"
       />
       <SocialCard
         icon={<RiotGamesIcon width="32" height="32" />}
         label={{ name: 'joemama', subname: '#001' }}
         color={colors.riotgames}
+      />
+      <SocialCard
+        icon={<SpotifyIcon width="32" height="32" />}
+        label={{ name: 'hana' }}
+        color={colors.spotify}
+        link="spotify://user/1sr15fcypw0cuw773o1pvgyuq"
       />
     </div>
   )
