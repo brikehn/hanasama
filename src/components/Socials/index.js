729797a7f41c85ca931a0f7fd13bf18934f68a35
brikehn <thebriankwon@gmail.com>
2022-01-26 21:59:41 -0500

    add Twitch

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -1,47 +1,55 @@
-import React from 'react'
-import './Socials.scss'
-import SocialCard from './SocialCard'
+import React from "react";
+import "./Socials.scss";
+import SocialCard from "./SocialCard";
 
-import { ReactComponent as DiscordIcon } from '../../assets/images/Discord-Logo-White.svg'
-import { ReactComponent as SteamIcon } from '../../assets/images/Steam-Logo.svg'
-import { ReactComponent as RiotGamesIcon } from '../../assets/images/Riot_Games-Logo.svg'
-import { ReactComponent as SpotifyIcon } from '../../assets/images/Spotify-Logo.svg'
+import { ReactComponent as DiscordIcon } from "../../assets/images/Discord-Logo-White.svg";
+import { ReactComponent as SteamIcon } from "../../assets/images/Steam-Logo.svg";
+import { ReactComponent as RiotGamesIcon } from "../../assets/images/Riot_Games-Logo.svg";
+import { ReactComponent as SpotifyIcon } from "../../assets/images/Spotify-Logo.svg";
+import { ReactComponent as TwitchIcon } from "../../assets/images/Twitch-Logo.svg";
 
 const colors = {
-  discord: '#5865F2',
-  steam: '#000000',
-  riotgames: '#d13639',
-  spotify: '#191414'
-}
+  discord: "#5865F2",
+  steam: "#000000",
+  riotgames: "#d13639",
+  spotify: "#191414",
+  twitch: "#a970fc",
+};
 
 const Socials = () => {
   return (
     <div className="socials">
       <SocialCard
         icon={<DiscordIcon width="32" height="32" />}
-        label={{ name: 'hana', subname: '#0001' }}
+        label={{ name: "hana", subname: "#0001" }}
         color={colors.discord}
         link="discord:///users/244251015069564938"
       />
       <SocialCard
+        icon={<TwitchIcon width="32" height="32" />}
+        label={{ name: "hanasama" }}
+        color={colors.twitch}
+        link="https://twitch.tv/hanasama"
+      />
+      <SocialCard
         icon={<SteamIcon width="32" height="32" />}
-        label={{ name: 'hanabanana' }}
+        label={{ name: "hanabanana" }}
         color={colors.steam}
         link="steam://openurl/https://steamcommunity.com/id/hanasama96"
       />
       <SocialCard
         icon={<RiotGamesIcon width="32" height="32" />}
-        label={{ name: 'joemama', subname: '#001' }}
+        label={{ name: "joemama", subname: "#001" }}
         color={colors.riotgames}
       />
       <SocialCard
         icon={<SpotifyIcon width="32" height="32" />}
-        label={{ name: 'hana' }}
+        label={{ name: "hana" }}
         color={colors.spotify}
         link="spotify://user/1sr15fcypw0cuw773o1pvgyuq"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
